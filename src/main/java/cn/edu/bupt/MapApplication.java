63d6514128ea9a0408e9e36db77ead2203412312
zy <yi950607@126.com>
2018-12-20 11:47:08 +0800

    修改启动文件

--- src/main/java/cn/edu/bupt/MapApplication.java
+++ src/main/java/cn/edu/bupt/MapApplication.java
@@ -4,8 +4,9 @@ import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @MapperScan("cn.edu.bupt.map.mapper")
 public class MapApplication {
 
