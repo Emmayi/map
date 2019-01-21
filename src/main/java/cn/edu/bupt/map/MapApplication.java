785a447d2c169a9d73c9a27fb7c105a2659f884e
zy <yi950607@126.com>
2019-01-21 16:50:13 +0800

    添加统一配置文件

--- src/main/java/cn/edu/bupt/map/MapApplication.java
+++ src/main/java/cn/edu/bupt/map/MapApplication.java
@@ -1,13 +1,15 @@
-package cn.edu.bupt;
+package cn.edu.bupt.map;
 
-import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @MapperScan("cn.edu.bupt.map.mapper")
+@PropertySource({"classpath:disconf.properties"})
+@ImportResource({"classpath:disconf.xml"})//引入disconf
 public class MapApplication {
 
 	public static void main(String[] args) {
