a8f4668e8c8235e737ff52b30348079ccf8d2f22
00pf00 <408445889@qq.com>
2019-01-22 18:11:47 +0800

    发送和销毁方法

--- src/main/java/cn/edu/bupt/map/util/WebsocketUtil.java
+++ src/main/java/cn/edu/bupt/map/util/WebsocketUtil.java
@@ -1,11 +1,18 @@
 package cn.edu.bupt.map.util;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import cn.edu.bupt.map.actor.ProducerActor;
+import cn.edu.bupt.map.exception.CloseException;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author litengfei
@@ -17,13 +24,26 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint(value = "/api/v1/map/websocket")
 @Component
 public class WebsocketUtil {
+
     @OnOpen
     public void onOPen(Session session){
+        //KafkaTemplate kafkaTemplate = (KafkaTemplate)SpringUtil.getBean("kafkaTemplate");
+        Map<String, List<String>> requestParameterMap = session.getRequestParameterMap();
+        System.out.println(requestParameterMap);
+        ActorSystem actorSystem = (ActorSystem)SpringUtil.getBean("actorSystem");
+        ActorRef pf = actorSystem.actorOf(Props.create(ProducerActor.class), "pf");
+        pf.tell(session.getId(),ActorRef.noSender());
+        System.out.println("************************************");
+        //System.out.println(kafkaTemplate);
+        System.out.println(actorSystem);
         System.out.println("连接成功");
     }
 
     @OnClose
     public void onClose(){
+        ActorSystem actorSystem = (ActorSystem)SpringUtil.getBean("actorSystem");
+        ActorRef pf = actorSystem.actorFor("akka://ActorSystem/user/pf");
+        pf.tell(new CloseException(),ActorRef.noSender());
         System.out.println("断开连接");
     }
 }
