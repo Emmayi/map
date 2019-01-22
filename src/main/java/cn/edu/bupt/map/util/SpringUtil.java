9231e2c92349a45afcc8e4e079a9993da95244f9
00pf00 <408445889@qq.com>
2019-01-22 10:52:24 +0800

    添加注解

--- src/main/java/cn/edu/bupt/map/util/SpringUtil.java
+++ src/main/java/cn/edu/bupt/map/util/SpringUtil.java
@@ -3,13 +3,15 @@ package cn.edu.bupt.map.util;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
 /**
  * @author litengfei
  * @email: 1tengfei_2017@sina.com
  * @time 2019/1/21 16:26
- * @Description 描述:
+ * @Description 描述: 获取springbean的util方法
  */
+@Component
 public class SpringUtil implements ApplicationContextAware {
     private static ApplicationContext applicationContext;
     @Override
