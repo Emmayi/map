fa9e3465124f79febeb6031373cfce0d72dd36cf
00pf00 <408445889@qq.com>
2019-01-24 09:23:03 +0800

    修改kafka数据的key值为当前时间的long类型

--- src/main/java/cn/edu/bupt/map/actor/ProducerActor.java
+++ src/main/java/cn/edu/bupt/map/actor/ProducerActor.java
@@ -6,6 +6,8 @@ import cn.edu.bupt.map.util.SpringUtil;
 import cn.edu.bupt.map.util.StringUtil;
 import org.springframework.kafka.core.KafkaTemplate;
 
+import java.util.Date;
+
 /**
  * @author litengfei
  * @email: 1tengfei_2017@sina.com
@@ -18,7 +20,7 @@ public class ProducerActor extends UntypedActor {
         if(message instanceof String){
             KafkaTemplate kafkaTemplate = (KafkaTemplate)SpringUtil.getBean("kafkaTemplate");
             String topic = StringUtil.getUserNameFromPath(getSelf().path().toString());
-            kafkaTemplate.send(topic,"0001",message);
+            kafkaTemplate.send(topic,String.valueOf((new Date()).getTime()),message);
         }else{
             if (message instanceof  CloseException ){
                 getContext().stop(getSelf());
