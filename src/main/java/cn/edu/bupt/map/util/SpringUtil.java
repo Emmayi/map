828f60b22cfbd5562a9ea851c024c4c62835300a
00pf00 <408445889@qq.com>
2019-01-24 20:02:18 +0800

    删除多余注释

--- src/main/java/cn/edu/bupt/map/util/SpringUtil.java
+++ src/main/java/cn/edu/bupt/map/util/SpringUtil.java
@@ -24,14 +24,6 @@ public class SpringUtil implements ApplicationContextAware {
      * @email: tengfei_2017@sina.com
      *
      *  简要描述方法的作用：通过名称获取bean
-
-     * (这里描述这个方法适用条件 – 可选)
-
-     * (这里描述这个方法的执行流程 – 可选)
-
-     * (这里描述这个方法的使用方法 – 可选)
-
-     * (这里描述这个方法的注意事项 – 可选)
      */
     public static Object getBean(String name) {
         return applicationContext.getBean(name);
