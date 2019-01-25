3289f81dd904f63a1dc9c7c2c6376c35d28b16ec
00pf00 <408445889@qq.com>
2019-01-25 09:43:07 +0800

    解决运行报错

--- src/main/script/consumer.js
+++ src/main/script/consumer.js
@@ -23,27 +23,28 @@ var topics = [{
 var consumer = new Consumer(client, topics, options);
 var offset = new Offset(client);
 consumer.on('message', function(message) {
-    var obj = message;
-    var message = JSON.parse(message.value);
-    var args = [];
-    console.log('start');
-    args.push(message);
-    emitter.emit('load', args);
+    console.log(message);
+    // var obj = message;
+    // var message = JSON.parse(message.value);
+    // var args = [];
+    // console.log('start');
+    // args.push(message);
+    // emitter.emit('load', args);
  
 });
  
-consumer.on('error', function(err) {
-    console.log('error', err);
-});
- 
-emitter.on('load', function(args) {
-        console.log('listener2', args);
- 
-});
-consumer.on('offsetOutOfRange', function(topic) {
-    topic.maxNum = 2;
-    offset.fetch([topic], function(err, offsets) {
-        var min = Math.min.apply(null, offsets[topic.topic][topic.partition]);
-        consumer.setOffset(topic.topic, topic.partition, min);
-    });
-});
+// consumer.on('error', function(err) {
+//     console.log('error', err);
+// });
+//
+// emitter.on('load', function(args) {
+//         console.log('listener2', args);
+//
+// });
+// consumer.on('offsetOutOfRange', function(topic) {
+//     topic.maxNum = 2;
+//     offset.fetch([topic], function(err, offsets) {
+//         var min = Math.min.apply(null, offsets[topic.topic][topic.partition]);
+//         consumer.setOffset(topic.topic, topic.partition, min);
+//     });
+// });
